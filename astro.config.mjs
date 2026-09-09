17fdfe9ef55b7eabc0cd190d24d7fcf12806154a
呂旻軒 <kevin@MacBook-Air-85.local>
2026-09-09 09:52:10 +0800

    Fix deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://a0953801252-cmyk.github.io',
+  base: '/ongngaping',
+});
